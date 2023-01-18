b6638bcc6adf11fdc400e4a5763d13cb14f465dc
Sanjay Soundarajan <sanjay.16@live.com>
2023-01-17 16:42:02 -0800

    chore: 🔨 update image

--- src/pages/modules/teaming.tsx
+++ src/pages/modules/teaming.tsx
@@ -22,7 +22,7 @@ const TeamingModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
 
         <HeroCroppedImage
           title='Teaming'
-          image='https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
+          image='https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'
           tagline='Applying and advancing team science while transforming our AI-READI project from a multidisciplinary endeavor into a high-performing, interdisciplinary collaboration.'
           showButtons={false}
           primaryButton='Learn more about the study'
