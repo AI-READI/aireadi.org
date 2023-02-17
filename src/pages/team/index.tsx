6d3b80525912c51e56fc316fd6e1875b93742599
Sanjay Soundarajan <sanjay.16@live.com>
2023-02-17 12:31:30 -0800

    fix: 🐛 adjust image width

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -94,7 +94,7 @@ const TeamPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
                   key={member.name}
                   id={member.id}
                 >
-                  <div className='relative h-full min-h-[500px] w-full md:min-h-fit md:w-2/5 '>
+                  <div className='relative h-full min-h-[500px] w-full md:min-h-fit md:w-5/12 '>
                     <Image
                       src={member.image}
                       alt='user profile picture'
@@ -106,7 +106,7 @@ const TeamPage: React.FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
                     />
                   </div>
 
-                  <div className='w-full py-8 px-6 text-left md:w-3/5 md:p-4'>
+                  <div className='w-full py-8 px-6 text-left md:w-7/12 md:p-4'>
                     <h3 className='text-xl font-extrabold text-gray-900'>
                       {member.name}{' '}
                       {member.pronoun && (
