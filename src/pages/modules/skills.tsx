df4fc44da6f1fcd909f58c4b56ec4f77b50291da
Sanjay Soundarajan <sanjay.16@live.com>
2023-09-01 10:44:03 -0700

    Revert "Bootcamp" (#30)

--- src/pages/modules/skills.tsx
+++ src/pages/modules/skills.tsx
@@ -100,42 +100,7 @@ const SkillsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
             </ul>
           </div>
         </section>
-        <section className='bg-slate-50'>
-          <div className='mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6'>
-            <div className='mx-auto mb-6 max-w-screen-sm text-center lg:mb-8'>
-              <h2 className='mb-1 text-3xl font-extrabold tracking-tight sm:text-4xl'>
-                Intern Orientation and Onboarding
-              </h2>
-            </div>
-            <p className='text-center text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-              Lorem ipsum, or lipsum as it is sometimes known, used in laying
-              out print, graphic or web designs. The passage is attributed to an
-              unknown typesetter in the 15th century who is thought to have
-              scrambled parts of Cicero's De Finibus Bonorum et Malorum for use
-              in a type specimen book. It usually begins with:
-            </p>
-            <div className='mx-auto flex max-w-screen-xl flex-col-reverse items-center gap-16 md:flex-col '>
-              <div className='mt-8 grid grid-cols-1 gap-4 md:grid-cols-3'>
-                <img
-                  className='rounded-lg'
-                  src='https://ucarecdn.com/b6521517-979e-4ae4-baff-69ade647a982/'
-                  alt='Photo of the UAB Callahan Eye Hospital'
-                />
 
-                <img
-                  className='rounded-lg'
-                  src='https://ucarecdn.com/43f384e1-ec90-410f-b3b5-7d72f3eda492/-/crop/16:9/bottom/'
-                  alt='Photo of the UCSD Medical Center'
-                />
-                <img
-                  className='rounded-lg'
-                  src='https://ucarecdn.com/2b081802-f0a0-40b5-b333-b1438e89415e/'
-                  alt='Photo of the UW Medicine building'
-                />
-              </div>
-            </div>
-          </div>
-        </section>
         <ModuleMembers Team={TeamMembers} />
       </main>
     </Layout>
