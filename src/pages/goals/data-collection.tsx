86d27109c6ca577cfbcd3d54f320c75f1377afac
Sanjay Soundarajan <sanjay.16@live.com>
2024-05-10 18:57:20 -0700

    wip: 🚧 goal pages

--- src/pages/goals/data-collection.tsx
+++ src/pages/goals/data-collection.tsx
@@ -2,28 +2,13 @@
 import { SkipNavContent, SkipNavLink } from '@chakra-ui/skip-nav';
 
 import HeroCroppedImage from '@/components/hero/HeroCroppedImage';
-import ResourcesItemIcon from '@/components/icons/ResourcesItemIcon';
 import Layout from '@/components/layout/Layout';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import ButtonLink from '@/components/links/ButtonLink';
+import PrimaryLink from '@/components/links/PrimaryLink';
 import ModuleMembers from '@/components/modules/ModuleMembers';
 import Seo from '@/components/Seo';
 
 import ModuleMembersFilter from '@/utils/moduleMembersFilter';
-
-const Resources = [
-  {
-    title: 'Data Acquisition Module',
-    description: 'A collection of resources to help you get started.',
-    link: '/data',
-    type: 'publication',
-  },
-  {
-    title: 'Github',
-    description: 'A collection of resources to help you get started.',
-    link: '/data',
-    type: 'github',
-  },
-];
 
 const EthicsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
   <>
@@ -37,8 +22,8 @@ const EthicsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
 
         <HeroCroppedImage
           title='Data Collection'
-          image='https://images.unsplash.com/photo-1518186285589-2f7649de83e0?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-          tagline='Managing the collection of data for the AI-READI project'
+          image='/images/modules/data-hero.svg'
+          tagline='Develop and implement a protocol that will generate a diverse and ethically-sourced dataset for the study of type 2 diabetes.'
           showButtons={false}
           primaryButton='Learn more about the study'
         />
@@ -47,67 +32,67 @@ const EthicsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
           <div className='mx-auto grid max-w-screen-xl grid-cols-1 gap-10 px-4 py-8 text-left md:grid-cols-2 lg:py-16'>
             <div>
               <h2 className='mb-4 text-3xl font-extrabold tracking-tight sm:text-4xl'>
-                Overview of the Data Acquisition Module
+                Multimodal Data from Diverse Participants
               </h2>
               <p className='text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-                The University of Alabama at Birmingham (UAB) will oversee and
-                coordinate data acquisition activities over 3 sites: UAB,
-                University of California San Diego (UCSD) and University of
-                Washington (UW).
+                The AI-READI project aims to collect data across diverse
+                participants. The data will be balanced across sexes (equal
+                number of male and female), ethnicities/races (equal number of
+                Asian, Black, Hispanic, and White), and health states (equal
+                number of non-diabetic, diet-controlled diabetic, oral
+                medication-controlled diabetic, and insulin-controlled
+                diabetic).
               </p>
             </div>
             <div className=''>
               <p className='mb-4 text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-                Data is responsible for:
+                To achieve that, data is being collected at three study sites:
+                University of Alabama at Birmingham (UAB), University of
+                California San Diego (UCSD), and University of Washington (UW).
+                The same study protocol is followed at each site where a wide
+                range of data types are collected from each participant
+                including survey responses, vision assessment results, retinal
+                imaging, and activity monitoring data. Extraction of electronic
+                health records (EHRs) is also planned.
               </p>
-              <ul className='list mb-3 list-inside list-disc text-lg font-normal text-gray-700'>
-                <li>Determining all domains and variables for the dataset.</li>
-                <li>Developing the protocol.</li>
-                <li>Drafting the Manual of Procedures (MOP).</li>
-                <li>
-                  Implementing the protocol for data collection, starting with
-                  the hiring, training, and certification of Clinical Research
-                  Coordinators (CRCs), followed by the enrollment of
-                  participants and participant visits.
-                </li>
-                <li>
-                  Overseeing the biorepository storage of biospecimens at UAB
-                  for future ancillary studies.
-                </li>
-              </ul>
             </div>
           </div>
         </section>
 
-        <section className='hidden bg-white'>
-          <div className='mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16'>
-            <div className='mx-auto mb-6 max-w-screen-sm text-center lg:mb-8'>
-              <h2 className='mb-1 text-3xl font-extrabold tracking-tight sm:text-4xl'>
-                Resources
-              </h2>
-            </div>
+        <section className='bg-sky-50 py-16'>
+          <div className='mx-auto flex max-w-screen-xl flex-col items-center justify-between px-4 lg:flex-row-reverse'>
+            <div className='px-5 lg:max-w-2xl'>
+              <h1 className='mb-4 text-3xl font-bold tracking-tight sm:text-4xl'>
+                Management and Processing of Data
+              </h1>
+              <p className='mb-6 text-lg text-gray-700 sm:mb-4'>
+                We are developing a novel platform called FAIRhub that is aimed
+                at facilitating the management and sharing of data being
+                collected in the project. The study management component of
+                FAIRhub (
+                <PrimaryLink href='https://app.fairhub.io'>
+                  app.fairhub.io
+                </PrimaryLink>
+                ) enables the data collecting sites to upload data on the go as
+                it is being collected and to track data collection through
+                dashboards. The platform also includes automation tools to
+                process data and extract metadata according to our selected
+                standards for making the data AI-ready (see Data Sharing page).
+              </p>
 
-            <ul>
-              {Resources.map((resource) => (
-                <li
-                  className='mb-8 flex items-start space-x-4'
-                  key={resource.title}
-                >
-                  <ResourcesItemIcon type={resource.type} />
-                  <div className='pt-2'>
-                    <UnstyledLink
-                      href={resource.link}
-                      className='flex items-center space-x-1 text-lg font-medium text-sky-700 transition-all hover:text-sky-500'
-                    >
-                      <p className='text-xl font-medium'>{resource.title}</p>
-                    </UnstyledLink>
-                    <p className='pt-1 text-lg font-normal text-gray-700'>
-                      {resource.description}
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
+              <div>
+                <ButtonLink href='/goals/data-sharing' variant='outline'>
+                  View our data sharing standards
+                </ButtonLink>
+              </div>
+            </div>
+            <div className='flex w-full items-center justify-center px-5 py-5'>
+              <img
+                className='rounded-lg'
+                src='https://images.unsplash.com/photo-1503428593586-e225b39bddfe?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                alt='mockup'
+              />
+            </div>
           </div>
         </section>
 
--- src/pages/goals/data-sharing.tsx
+++ src/pages/goals/data-sharing.tsx
@@ -2,28 +2,12 @@
 import { SkipNavContent, SkipNavLink } from '@chakra-ui/skip-nav';
 
 import HeroCroppedImage from '@/components/hero/HeroCroppedImage';
-import ResourcesItemIcon from '@/components/icons/ResourcesItemIcon';
 import Layout from '@/components/layout/Layout';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import PrimaryLink from '@/components/links/PrimaryLink';
 import ModuleMembers from '@/components/modules/ModuleMembers';
 import Seo from '@/components/Seo';
 
 import ModuleMembersFilter from '@/utils/moduleMembersFilter';
-
-const Resources = [
-  {
-    title: 'Data Acquisition Module',
-    description: 'A collection of resources to help you get started.',
-    link: '/data',
-    type: 'publication',
-  },
-  {
-    title: 'Github',
-    description: 'A collection of resources to help you get started.',
-    link: '/data',
-    type: 'github',
-  },
-];
 
 const EthicsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
   <>
@@ -44,59 +28,139 @@ const EthicsModule: React.FC<ModuleMembersPageProps> = ({ TeamMembers }) => (
         />
 
         <section className='bg-slate-50'>
-          <div className='mx-auto grid max-w-screen-xl grid-cols-1 gap-10 px-4 py-8 text-left md:grid-cols-2 lg:py-16'>
+          <div className='mx-auto grid max-w-screen-xl grid-cols-1 gap-10 px-4 pt-8 text-left md:grid-cols-2 lg:pt-16'>
+            <h2 className='mb-4 text-3xl font-extrabold tracking-tight sm:text-4xl'>
+              Data Standardization
+            </h2>
+          </div>
+          <div className='mx-auto grid max-w-screen-xl grid-cols-1 gap-10 px-4 pb-8 text-left md:grid-cols-2 lg:pb-16'>
             <div>
-              <h2 className='mb-4 text-3xl font-extrabold tracking-tight sm:text-4xl'>
-                Multidisciplinary Team Science
-              </h2>
               <p className='text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-                The goal of the ETAI module is to establish a culture of ethical
-                inquiry throughout the data generation project team.
-              </p>
-              <p className='mb-4 text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-                ETAI will have continuous interactions with the other five
-                modules within AI-READI and with the Bridge2AI Center.
+                We aim to share the AI-READI dataset such that it is easily
+                reusable by anyone, especially for developing artificial
+                intelligence (AI) and machine learning (ML) models. To achieve
+                that, we are formatting our data according to existing standards
+                or establishing new ones when there is a gap. Specifically, we
+                are developing the{' '}
+                <PrimaryLink href='https://cds-specification.readthedocs.io/'>
+                  Clinical Dataset Structure (CDS)
+                </PrimaryLink>
+                , a standard for organizing multimodal clinical research data
+                and metadata consistently, and following it to organize the
+                AI-READI dataset.
               </p>
             </div>
-            <div>
+            <div className=''>
               <p className='mb-4 text-gray-700 sm:mb-4 sm:text-xl md:text-lg'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
-                amet facilisis lectus. Nullam nec erat in nulla aliquet
-                scelerisque. Nulla facilisi. Sed sit amet facilisis lectus.
+                Additionally, each data modality is formatted according to
+                existing standard formats such as the{' '}
+                <PrimaryLink href='https://www.ohdsi.org/data-standardization/'>
+                  Observational Medical Outcomes Partnership (OMOP)
+                </PrimaryLink>{' '}
+                Common Data Model (CDM) for clinical data and the{' '}
+                <PrimaryLink href='https://dicom.nema.org/'>
+                  Digital Imaging and Communications in Medicine (DICOM)
+                </PrimaryLink>{' '}
+                format for retinal imaging data. We are also working with the
+                communities managing these standards to extend them when needed
+                for our data.
               </p>
             </div>
           </div>
         </section>
 
-        <section className='hidden bg-white'>
-          <div className='mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16'>
-            <div className='mx-auto mb-6 max-w-screen-sm text-center lg:mb-8'>
-              <h2 className='mb-1 text-3xl font-extrabold tracking-tight sm:text-4xl'>
-                Resources
-              </h2>
+        <section className='bg-sky-50 py-16'>
+          <div className='mx-auto flex max-w-screen-xl flex-col items-center justify-between px-4 lg:flex-row-reverse'>
+            <div className='px-5 lg:max-w-2xl'>
+              <h1 className='mb-4 text-3xl font-bold tracking-tight sm:text-4xl'>
+                New Data Usage License
+              </h1>
+              <p className='mb-6 text-lg text-gray-700 sm:mb-4'>
+                We have established a{' '}
+                <PrimaryLink href='https://doi.org/10.5281/zenodo.10642459'>
+                  new data usage license
+                </PrimaryLink>{' '}
+                for the AI-READI dataset. The license terms were specifically
+                tailored to enable reuse of the AI-READI dataset (and other
+                clinical datasets) for commercial or research purposes while
+                putting strong requirements around data usage, security, and
+                secondary sharing to protect study participants, especially when
+                data is reused for AI/ML related applications. The license will
+                continue to evolve as we receive feedback from the community
+                based on the release of our pilot dataset (see below).
+              </p>
             </div>
+            <div className='flex w-full items-center justify-center px-5 py-5'>
+              <img
+                className='rounded-lg'
+                src='https://images.unsplash.com/photo-1503428593586-e225b39bddfe?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                alt='mockup'
+              />
+            </div>
+          </div>
+        </section>
 
-            <ul>
-              {Resources.map((resource) => (
-                <li
-                  className='mb-8 flex items-start space-x-4'
-                  key={resource.title}
-                >
-                  <ResourcesItemIcon type={resource.type} />
-                  <div className='pt-2'>
-                    <UnstyledLink
-                      href={resource.link}
-                      className='flex items-center space-x-1 text-lg font-medium text-sky-700 transition-all hover:text-sky-500'
-                    >
-                      <p className='text-xl font-medium'>{resource.title}</p>
-                    </UnstyledLink>
-                    <p className='pt-1 text-lg font-normal text-gray-700'>
-                      {resource.description}
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
+        <section className=' py-16'>
+          <div className='mx-auto flex max-w-screen-xl flex-col items-center justify-between px-4 lg:flex-row'>
+            <div className='px-5 lg:max-w-2xl'>
+              <h1 className='mb-4 text-3xl font-bold tracking-tight sm:text-4xl'>
+                Data Use Agreement
+              </h1>
+              <p className='mb-6 text-lg text-gray-700 sm:mb-4'>
+                At each release of the AI-READI dataset, two sets will be made
+                available: a public access and a controlled access set. The
+                public set will be stripped of Protected Health Information
+                (PHI) as well as information related to the sex and
+                race/ethnicity of the participants. It will be accessible after
+                completing a few steps through our data portal (see below) such
+                as logging in, providing the research purpose, and attesting to
+                the proper usage of the data. The controlled set of the dataset
+                will also be accessible through our data portal but will require
+                additional steps for accessing. We will develop specific
+                guidelines for accessing and using the controlled set of the
+                dataset. We will draw on best practices from other large data
+                programs that have controlled access data policies such as the
+                All of Us Research Program.
+              </p>
+            </div>
+            <div className='flex w-full items-center justify-center px-5 py-5'>
+              <img
+                className='rounded-lg'
+                src='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                alt='mockup'
+              />
+            </div>
+          </div>
+        </section>
+
+        <section className='bg-slate-50 py-16'>
+          <div className='mx-auto flex max-w-screen-xl flex-col items-center justify-between px-4 lg:flex-row-reverse'>
+            <div className='px-5 lg:max-w-2xl'>
+              <h1 className='mb-4 text-3xl font-bold tracking-tight sm:text-4xl'>
+                Data Access Portal
+              </h1>
+              <p className='mb-6 text-lg text-gray-700 sm:mb-4'>
+                We are developing a novel platform called FAIRhub for managing
+                and sharing the data. It will contain two components: a study
+                management platform (app.fairhub.io) for managing data and a
+                data portal (fairhub.io) where data will be accessible to others
+                after it is shared from the study management platform. The data
+                portal is designed to make the data Findable and Accessible. The
+                public set of the AI-READI pilot dataset is already available
+                through FAIRhub at{' '}
+                <PrimaryLink href='https://doi.org/10.60775/fairhub.1'>
+                  https://doi.org/10.60775/fairhub.1
+                </PrimaryLink>
+                .
+              </p>
+            </div>
+            <div className='flex w-full items-center justify-center px-5 py-5'>
+              <img
+                className='rounded-lg'
+                src='https://images.unsplash.com/photo-1503428593586-e225b39bddfe?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                alt='mockup'
+              />
+            </div>
           </div>
         </section>
 
