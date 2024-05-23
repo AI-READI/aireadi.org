d275405cf8e09699bd44bee68b6287edb3ccddf4
Sanjay Soundarajan <sanjay.16@live.com>
2024-05-23 11:46:17 -0700

    feat: ✨ add a contact form

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -40,6 +40,8 @@ export default function ContactUs() {
 
   return (
     <>
+      <script async src='https://tally.so/widgets/embed.js'></script>
+
       <SkipNavLink>Skip to content</SkipNavLink>
 
       <Layout>
@@ -54,15 +56,26 @@ export default function ContactUs() {
                 Contact Us
               </h1>
 
-              <p className='mb-6 hidden text-center text-xl font-normal text-slate-600 sm:mb-8'>
+              <p className='mb-6 text-center text-xl font-normal text-slate-600 sm:mb-8'>
                 Have a question about our project? Want more information about
                 our study? Need help using our data? Let us know!
               </p>
 
-              <p className='my-12 w-full rounded-lg bg-yellow-50 p-10 text-center text-xl font-normal text-slate-600 sm:mb-8'>
+              <p className='my-12 hidden w-full rounded-lg bg-yellow-50 p-10 text-center text-xl font-normal text-slate-600 sm:mb-8'>
                 We are not taking any queries at this time. <br /> We hope you
                 can find all the necessary information on our website.
               </p>
+
+              <iframe
+                data-tally-src='https://tally.so/embed/wdY71y?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1'
+                loading='lazy'
+                width='100%'
+                height='200'
+                frameborder='0'
+                marginheight='0'
+                marginwidth='0'
+                title='Contact form'
+              ></iframe>
 
               <Formik
                 initialValues={{
