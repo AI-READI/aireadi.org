c6e3ae4d6d09111ae0e85aaa8195ca063be3d433
aydawka <aydan.gasimova2@gmail.com>
2025-04-04 14:55:45 -0700

    fix: text

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className='pt-[61px]'>{children}</div>
 
       <div className='fixed bottom-0 z-50 w-screen bg-sky-200 p-2 text-center text-sm text-gray-900 max-sm:bottom-2'>
-        This repository is under review for potential modification in compliance
+        This dataset is under review for potential modification in compliance
         with Administration directives.
       </div>
       <Footer />
