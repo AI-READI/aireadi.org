e554af8d8c7011ddbbcbc608b088921e9bb70c04
aydawka <aydan.gasimova2@gmail.com>
2026-05-19 21:35:25 -0700

    chore: update in-person participants

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -267,7 +267,7 @@ const HomePage: React.FC<EventItem> = ({ slug, frontMatter }) => {
                       variants={FadeFramerItem}
                       className='completed-study-text'
                     >
-                      <span className='block text-xl font-bold'>3,905+</span>
+                      <span className='block text-xl font-bold'>3,935+</span>
                       <span className='max-w-[200px] text-[16px] font-normal max-md:max-w-[140px]'>
                         participants completed in-person study visit
                       </span>
